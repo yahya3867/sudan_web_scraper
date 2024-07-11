959c168a66c3ebddebeb28c5fa2d6a29b2bc6588
Handanovic1 <84147408+Handanovic1@users.noreply.github.com>
2024-07-11 13:55:36 -0400

    Update xinhua.py

--- Crawlers/xinhua.py
+++ Crawlers/xinhua.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
         articles = scrape_article(1)
     else:
         print('Running in initial mode')
-        last_page = find_last_relevant_page()
+        last_page = 118
         for i in range(1, last_page+1):
             print(f'Processing page {i} of {last_page}')
             articles += scrape_article(1)
