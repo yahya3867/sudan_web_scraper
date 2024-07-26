cfd0c177a1be5001fa55a867547dda027b90907d
Handanovic1 <84147408+Handanovic1@users.noreply.github.com>
2024-07-26 12:05:21 -0400

    Create arab_news.py

---
+++ Crawlers/arab_news.py
@@ -0,0 +1,113 @@
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime, date
+import os
+import itertools
+from scraping_tools import store_articles, store_most_recent, store_article_analytics
+from dotenv import load_dotenv
+import sys
+import html
+import dateparser
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+
+
+load_dotenv()
+
+DEPLOYMENT = os.getenv('DEPLOYMENT')
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'initial':
+        DEPLOYMENT = False
+
+URL = f'https://www.arabnews.com/tags/sudan'
+SOURCE = 'Arab News'
+current_date = date.today()
+
+# finds all the articles that relates to the Sudan war, war crimes, violence, etc
+def scrape_articles():
+    chrome_options = Options()
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--ignore-ssl-errors')
+    chrome_options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.arabnews.com/tags/sudan") 
+
+    db_list = []
+    try:
+        driver.get(URL) 
+
+        articles = driver.find_elements(By.CSS_SELECTOR, 'div.article-item-info')
+
+        headlines = []
+        urls = []
+        dates = []
+
+
+        for article in articles:
+            element = article.find_element(By.TAG_NAME, 'a')
+            
+            headlines.append(element.text.strip())
+            
+            urls.append(element.get_attribute('href'))
+
+            times = article.find_element(By.TAG_NAME,'time').text
+            timestrings = [str(times)]
+            a_date = ''
+
+            for timestring in timestrings:
+                dt = dateparser.parse(timestring)
+                a_date = dt.strftime("%Y-%m-%d")
+                dates.append(a_date)
+
+        img = []
+        body_list = []
+
+        for i in urls:
+            time.sleep(2)
+
+            new_driver = webdriver.Chrome(options=chrome_options)
+            new_driver.get(i)
+
+            try:
+                article = new_driver.find_element(By.TAG_NAME,'article')
+
+                big_body = [i.text for i in article.find_elements(By.TAG_NAME, 'p')]
+                body = ''
+                for i in range(1, len(big_body)):
+                    body += big_body[i]
+                    body += ' '
+
+                body_list.append(body)
+
+                img.append(article.find_element(By.TAG_NAME,'img').get_attribute('src'))
+
+            except:
+                pass
+            finally:
+                new_driver.quit()
+        print(len(headlines),len(urls),len(dates),len(body_list),len(img))
+        for i in range(len(headlines)):
+            db_data = {'source': SOURCE,
+                    'headline': headlines[i],
+                    'web_url': urls[i],
+                    'date': dates[i],
+                    'body': body_list[i],
+                    'image_urls': img[i],
+                    'archive_date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                }
+            db_list.append(db_data)
+    except:
+        pass
+    finally:
+        driver.quit()
+
+    return db_list
+
